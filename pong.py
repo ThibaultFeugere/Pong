55532388b8495616f57003353a37ed245ff4f3c7
Tluabiht <thibaultfeugere@gmail.com>
2018-12-17 19:56:44 +0100

    Création du canevas et placement du composant. Import de la lib time et attribution de la hauteur et la largeur non-redimensionnable.

--- pong.py
+++ pong.py
@@ -1,8 +1,18 @@
 from tkinter import *
 from random import *
+from time import *
 
+hauteur = 500
+largeur = 900
+
+# Creation des composants
 fenetre = Tk()
 fenetre.title('Pong - Thibault Feugère')
+fenetre.resizable(False,False)
+canevas = Canvas(fenetre, width=largeur, height=hauteur, bg='black')
+
+# Placement des composants
+canevas.grid()
 
 # Boucle infinie
 fenetre.mainloop()
