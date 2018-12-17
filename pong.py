5a30b114ea97de2f47cc9f00e2b71f0f842fbe94
Tluabiht <thibaultfeugere@gmail.com>
2018-12-17 21:07:25 +0100

    Mise en place du système de rebond

--- pong.py
+++ pong.py
@@ -16,8 +16,8 @@ fenetre.resizable(False,False)
 # Creation de la balle
 x0 = 440
 y0 = 240
-v_x = 0.2
-v_y = 0.15
+v_x = 0.4
+v_y = 0.1
 balle = canevas.create_oval(x0, y0, (x0 + 20), (y0 + 20), fill="white", width=0, state="disabled")
 
 # Definition de la fonction de mouvement
@@ -28,7 +28,17 @@ def mouvement():
     y0 += v_y
 
     canevas.coords(balle, x0, y0, (x0 + 20), (y0 + 20))
-    canevas.after(5, mouvement)
+
+    if (x0 + 20) > largeur:
+        v_x = -(v_x)
+    elif x0 < 0:
+        v_x = -(v_x)
+    elif (y0 + 20) > hauteur:
+        v_y = -(v_y)
+    elif y0 < 0:
+        v_y = -(v_y)
+
+    canevas.after(2, mouvement)
     return
 
 # Placement des composants
