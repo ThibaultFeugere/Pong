30ffc1ca51984c08587a16b738078199dd7b1563
Tluabiht <thibaultfeugere@gmail.com>
2018-12-17 20:53:47 +0100

    Mise en place du système de mouvement infini

--- pong.py
+++ pong.py
@@ -14,10 +14,26 @@ canevas = Canvas(fenetre, width=largeur, height=hauteur, bg='black')
 fenetre.resizable(False,False)
 
 # Creation de la balle
-balle = canevas.create_oval(440, 240, 460, 260, fill="white", width=0, state="disabled")
+x0 = 440
+y0 = 240
+v_x = 0.2
+v_y = 0.15
+balle = canevas.create_oval(x0, y0, (x0 + 20), (y0 + 20), fill="white", width=0, state="disabled")
+
+# Definition de la fonction de mouvement
+
+def mouvement():
+    global x0, y0, v_x, v_y
+    x0 += v_x
+    y0 += v_y
+
+    canevas.coords(balle, x0, y0, (x0 + 20), (y0 + 20))
+    canevas.after(5, mouvement)
+    return
 
 # Placement des composants
 canevas.grid()
+mouvement()
 
 # Boucle infinie
 fenetre.mainloop()
