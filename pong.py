5c1f5a1956b3c2fe0a1240a5d18a6e60af75cd8a
Tluabiht <thibaultfeugere@gmail.com>
2018-12-29 16:58:20 +0100

    Amélioration de l'affichage des scores, ajout de la fonctionnalité Start/pause

--- pong.py
+++ pong.py
@@ -18,9 +18,6 @@ fenetre = Tk()
 fenetre.title('Pong - Thibault Feugère')
 canevas = Canvas(fenetre, width=largeur, height=hauteur, bg='black')
 
-# Affichage score
-score_1 = Text(fenetre, width="100", height="20", borderwidth="3", font="Verdana 11", bg="#ffffff")
-
 # Delimitation
 barre_verticale = canevas.create_line((largeur/2), 0, (largeur/2), hauteur, fill="red", dash=(2,2))
 
@@ -30,37 +27,38 @@ fenetre.resizable(False,False)
 # Creation de la balle
 balle_x0 = 440
 balle_y0 = 240
-balle_v_x = 0.4
-balle_v_y = 0.1
+balle_v_x = 0.2
+balle_v_y = 0.2
 balle = canevas.create_oval(balle_x0, balle_y0, (balle_x0 + 20), (balle_y0 + 20), fill="white", width=0, state="disabled")
 
 # Creation raquette du joueur un
 raq1_x0 = 5
 raq1_y0 = 215
-raq1_v_y = 5
+raq1_v_y = 18
 raquette1 = canevas.create_rectangle(raq1_x0, raq1_y0, (raq1_x0 + 14), (raq1_y0 + 70), fill="white", width=0, state="disabled")
 
 # Creation raquette du joueur deux
 raq2_x0 = largeur - 4
 raq2_y0 = 215
-raq2_v_y = 5
+raq2_v_y = 18
 raquette2 = canevas.create_rectangle(raq2_x0, raq2_y0, (raq2_x0 - 14), (raq2_y0 + 70), fill="white", width=0, state="disabled")
 
 ### Fonctions ###
 
 # Fonction mouvement infini
 def mouvement():
-    global balle_x0, balle_y0, balle_v_x, balle_v_y, score1, score2, game_is_launch
-    '''if game_is_launch == True:'''
-    balle_x0 += balle_v_x
-    balle_y0 += balle_v_y
+    global balle_x0, balle_y0, balle_v_x, balle_v_y, game_is_launch
 
-    canevas.coords(balle, balle_x0, balle_y0, (balle_x0 + 20), (balle_y0 + 20))
+    if game_is_launch == True:
+        balle_x0 += balle_v_x
+        balle_y0 += balle_v_y
+        canevas.coords(balle, balle_x0, balle_y0, (balle_x0 + 20), (balle_y0 + 20))
+
     canevas.after(2, mouvement)
     return
 
 # Fonction de verification avec score
-def verif():
+def verif_balle():
     global balle_x0, balle_y0, balle_v_x, balle_v_y, raq1_x0, raq1_y0, raq1_v_y, score1, score2
     if (balle_x0 + 20) > largeur:
         balle_v_x = -(balle_v_x)
@@ -70,50 +68,73 @@ def verif():
         score2 += 1
     elif (balle_y0 + 20) > hauteur or balle_y0 < 0:
         balle_v_y = -(balle_v_y)
-    canevas.after(2, verif)
+    canevas.after(2, verif_balle)
 
 # Fonctions de commande
 def z_key(event):
-    global raq1_x0, raq1_y0, raq1_v_y
-    raq1_y0 -= raq1_v_y
-    canevas.coords(raquette1, raq1_x0, raq1_y0, (raq1_x0 + 14), (raq1_y0 + 70))
+    global raq1_x0, raq1_y0, raq1_v_y, game_is_launch
+    if game_is_launch == True:
+        raq1_y0 -= raq1_v_y
+        canevas.coords(raquette1, raq1_x0, raq1_y0, (raq1_x0 + 14), (raq1_y0 + 70))
+        return raq1_y0
 
 def s_key(event):
-    global raq1_x0, raq1_y0, raq1_v_y
-    raq1_y0 += raq1_v_y
-    canevas.coords(raquette1, raq1_x0, raq1_y0, (raq1_x0 + 14), (raq1_y0 + 70))
+    global raq1_x0, raq1_y0, raq1_v_y, game_is_launch
+    if game_is_launch == True:
+        raq1_y0 += raq1_v_y
+        canevas.coords(raquette1, raq1_x0, raq1_y0, (raq1_x0 + 14), (raq1_y0 + 70))
 
 def up_key(event):
-    global raq2_x0, raq2_y0, raq2_v_y
-    raq2_y0 -= raq2_v_y
-    canevas.coords(raquette2, raq2_x0, raq2_y0, (raq2_x0 - 14), (raq2_y0 + 70))
+    global raq2_x0, raq2_y0, raq2_v_y, game_is_launch
+    if game_is_launch == True:
+        raq2_y0 -= raq2_v_y
+        canevas.coords(raquette2, raq2_x0, raq2_y0, (raq2_x0 - 14), (raq2_y0 + 70))
 
 def down_key(event):
-    global raq2_x0, raq2_y0, raq2_v_y
-    raq2_y0 += raq2_v_y
-    canevas.coords(raquette2, raq2_x0, raq2_y0, (raq2_x0 - 14), (raq2_y0 + 70))
+    global raq2_x0, raq2_y0, raq2_v_y, game_is_launch
+    if game_is_launch == True:
+        raq2_y0 += raq2_v_y
+        canevas.coords(raquette2, raq2_x0, raq2_y0, (raq2_x0 - 14), (raq2_y0 + 70))
+
+def verif_raquettes():
+    global raq1_x0, raq2_x0, raq1_y0, raq2_y0, hauteur
+    if raq1_y0 < 0:
+        canevas.coords(raquette1, raq1_x0, 0, (raq1_x0 + 14), 70)
+    elif raq1_y0 > hauteur:
+        canevas.coords(raquette1, raq1_x0, 430, (raq1_x0 + 14), hauteur)
+    elif raq2_y0 < 0:
+        canevas.coords(raquette2, raq2_x0, 0, (raq2_x0 - 14), 70)
+    elif raq2_y0 > hauteur:
+        canevas.coords(raquette2, raq2_x0, 430, (raq2_x0 - 14), hauteur)
 
 # Definition de la fonction start / pause
-'''
 def launch():
     global game_is_launch
     game_is_launch = True
-    return game_is_launch
-'''
+
+def pause():
+    global game_is_launch
+    game_is_launch = False
 
 # Creation du menu
-#start = Button(fenetre, text="Commencer", command=launch)
+start = Button(fenetre, text="Commencer", command=launch)
+pause = Button(fenetre, text="Pause", command=pause)
 quitter = Button(fenetre, text="Quitter", command=fenetre.quit)
+
+score_1 = canevas.create_text(370, 20, text="Joueur 1 : "+str(score1), fill="white", font="Verdana 11")
+score_2 = canevas.create_text(530, 20, text="Joueur 2 : "+str(score2), fill="white", font="Verdana 11")
 
 # Placement des composants
 canevas.grid()
-#start.grid()
+start.grid()
+pause.grid()
 quitter.grid()
+verif_raquettes()
 mouvement()
-verif()
-score_1.insert(END, "Joueur 1:" + str(score1))
-score_1.grid()
+verif_balle()
 
+
+# Detection de touches pressees
 canevas.bind_all('z', z_key)
 canevas.bind_all('s', s_key)
 canevas.bind_all('<Up>', up_key)
@@ -121,6 +142,3 @@ canevas.bind_all('<Down>', down_key)
 
 # Boucle infinie
 fenetre.mainloop()
-
-print('''Le joueur 1 a''', score1, '''point(s)''')
-print('''Le joueur 2 a''', score2, '''point(s)''')
